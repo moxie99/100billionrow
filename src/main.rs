263ef0a198df1e79a0e031d03ed29c0daa5d540d
Adeolu Oluwasegun <adeolusegun1000@gmail.com>
2026-02-26 22:50:22 +0100

    started the 100 billion row

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::{
+    collections::BTreeMap,
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let f = File::open("measurements.txt").unwrap();
+    let f = BufReader::new(f);
+    let mut stats = BTreeMap::<String, (f64, f64, usize, f64)>::new();
+    for line in f.lines() {
+        let line = line.unwrap();
+        let (station, temperature) = line.split_once(";").unwrap();
+        let temperature: f64 = temperature.parse().unwrap();
+        let stats = stats.entry(station.to_string()).or_default();
+        stats.0 = stats.0.min(temperature);
+        stats.1 += temperature;
+        stats.2 += 1;
+        stats.3 += stats.3.max(temperature);
+    }
+
+    print!("{{");
+
+    let mut stats = stats.into_iter().peekable();
+    while let Some((station, (min, sum, count, max))) = stats.next() {
+        print!("{station}={min}/{}/{max}", sum / (count as f64));
+        if stats.peek().is_some() {
+            print!(", ")
+        }
+    }
+    print!("}}");
 }
